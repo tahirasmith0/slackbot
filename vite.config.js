4188c9d41dd854298b8abde69e47db83dcde05b8
Tahira Smith <tahirasmith@crc-dot1x-nat-10-239-231-224.bu.edu>
2025-04-09 12:22:49 -0400

    pushing final changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-// /vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'frontend',
+  root: 'frontend', // Your source directory
+  build: {
+    outDir: 'frontend/dist', // Specify output directory for Vercel to find
+  },
   server: {
     port: 5173,
   },
